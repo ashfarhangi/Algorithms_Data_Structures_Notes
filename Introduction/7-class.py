9613cd0a0ffcd44357b09f01020f2e50c35f41ec
Ashkan Farhangi <enterthebeginning@gmail.com>
2019-07-22 20:19:46 -0400

    Intro fin

---
+++ Introduction/7-class.py
@@ -0,0 +1,69 @@
+class Person:
+    def __init__(self, name, age, month):
+        self.name = name
+        self.age = age
+        self.birthday_month = month
+        
+    def birthday(self):
+        self.age += 1
+
+def create_person_objects(names, ages, months):
+    my_data = zip(names, ages, months)
+    person_objects = []
+    for item in my_data:
+        person_objects.append(Person(*item))
+    return person_objects
+
+def get_april_birthdays(people):
+    # TODO:
+    # Increment "age" for all people with birthdays in April.
+    # Return a dictionary "april_birthdays" with the names of
+    # all people with April birthdays as keys, and their updated ages 
+    # as values. See the test below for an example expected output.
+    april_birthdays = {}
+    for person in people:
+        if person.birthday_month == 'April':
+            person.age += 1
+            april_birthdays[person.name] = person.age
+            
+    return april_birthdays
+
+def get_most_common_month(people):
+    # TODO: Use the "months" dictionary to record counts of 
+    # birthday months for persons in the "people" data.
+    # Return the month with the largest number of birthdays.
+    months = {'January':0, 'February':0, 'March':0, 'April':0, 'May':0, 
+              'June':0, 'July':0, 'August':0, 'September':0, 'October':0,
+              'November':0, 'December':0}
+
+    for person in people:
+        months[person.birthday_month] += 1
+
+    max_month = None
+    max_value = 0
+    for key in months.keys():
+        if months[key] > max_value:
+            max_value = months[key]
+            max_month = key
+
+    return max_month
+
+
+
+def test():
+    # Here is the data for the test. Assume there is a single most common month.
+    names = ['Howard', 'Richard', 'Jules', 'Trula', 'Michael', 'Elizabeth', 'Richard', 'Shirley', 'Mark', 'Brianna', 'Kenneth', 'Gwen', 'William', 'Rosa', 'Denver', 'Shelly', 'Sammy', 'Maryann', 'Kathleen', 'Andrew', 'Joseph', 'Kathleen', 'Lisa', 'Viola', 'George', 'Bonnie', 'Robert', 'William', 'Sabrina', 'John', 'Robert', 'Gil', 'Calvin', 'Robert', 'Dusty', 'Dario', 'Joeann', 'Terry', 'Alan', 'Rosa', 'Jeane', 'James', 'Rachel', 'Tu', 'Chelsea', 'Andrea', 'Ernest', 'Erica', 'Priscilla', 'Carol', 'Michael', 'Dale', 'Arthur', 'Helen', 'James', 'Donna', 'Patricia', 'Betty', 'Patricia', 'Mollie', 'Nicole', 'Ernest', 'Wendy', 'Graciela', 'Teresa', 'Nicole', 'Trang', 'Caleb', 'Robert', 'Paul', 'Nieves', 'Arleen', 'Milton', 'James', 'Lawrence', 'Edward', 'Susan', 'Patricia', 'Tana', 'Jessica', 'Suzanne', 'Darren', 'Arthur', 'Holly', 'Mary', 'Randal', 'John', 'Laura', 'Betty', 'Chelsea', 'Margaret', 'Angel', 'Jeffrey', 'Mary', 'Donald', 'David', 'Roger', 'Evan', 'Danny', 'William']
+    ages  = [17, 58, 79, 8, 10, 57, 4, 98, 19, 47, 81, 68, 48, 13, 39, 21, 98, 51, 49, 12, 24, 78, 36, 59, 3, 87, 94, 85, 43, 69, 15, 52, 57, 36, 52, 5, 52, 5, 33, 10, 71, 28, 70, 9, 25, 28, 76, 71, 22, 35, 35, 100, 9, 95, 69, 52, 66, 91, 39, 84, 65, 29, 20, 98, 30, 83, 30, 15, 88, 89, 24, 98, 62, 94, 86, 63, 34, 23, 23, 19, 10, 80, 88, 67, 17, 91, 85, 97, 29, 7, 34, 38, 92, 29, 14, 52, 94, 62, 70, 22]
+    months = ['January', 'March', 'January', 'October', 'April', 'February', 'August', 'January', 'June', 'August', 'February', 'May', 'March', 'June', 'February', 'August', 'June', 'March', 'August', 'April', 'April', 'June', 'April', 'June', 'February', 'September', 'March', 'July', 'September', 'December', 'June', 'June', 'August', 'November', 'April', 'November', 'August', 'June', 'January', 'August', 'May', 'March', 'March', 'March', 'May', 'September', 'August', 'April', 'February', 'April', 'May', 'March', 'March', 'January', 'August', 'October', 'February', 'November', 'August', 'June', 'September', 'September', 'January', 'September', 'July', 'July', 'December', 'June', 'April', 'February', 'August', 'September', 'August', 'February', 'April', 'July', 'May', 'November', 'December', 'February', 'August', 'August', 'September', 'December', 'February', 'March', 'June', 'December', 'February', 'May', 'April', 'July', 'March', 'June', 'December', 'March', 'July', 'May', 'September', 'November']
+    people = create_person_objects(names, ages, months)
+
+    # Calls to the two functions you have completed.
+    print(get_april_birthdays(people))
+    print(get_most_common_month(people))
+
+
+
+test()
+# Expected result:
+# {'Michael': 11, 'Erica': 72, 'Carol': 36, 'Lisa': 37, 'Lawrence': 87, 'Joseph': 25, 'Margaret': 35, 'Andrew': 13, 'Dusty': 53, 'Robert': 89}
+# August
