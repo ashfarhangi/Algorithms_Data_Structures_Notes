0120076782afcbcc57b55424562d855d0809cef5
Ashkan Farhangi <enterthebeginning@gmail.com>
2019-07-22 20:04:41 -0400

    Day 2

---
+++ Introduction/6-Sudoku.py
@@ -0,0 +1,67 @@
+correct = [[1,2,3],
+           [2,3,1],
+           [3,1,2]]
+
+incorrect = [[1,2,3,4],
+             [2,3,1,3],
+             [3,1,2,3],
+             [4,4,4,4]]
+
+incorrect2 = [[1,2,3,4],
+             [2,3,1,4],
+             [4,1,2,3],
+             [3,4,1,2]]
+
+incorrect3 = [[1,2,3,4,5],
+              [2,3,1,5,6],
+              [4,5,2,1,3],
+              [3,4,5,2,1],
+              [5,6,4,3,2]]
+
+incorrect4 = [['a','b','c'],
+              ['b','c','a'],
+              ['c','a','b']]
+
+incorrect5 = [ [1, 1.5],
+               [1.5, 1]]
+
+def check_sudoku(square):
+    for row in square:
+        # Create a list with the integers 1, 2, ..., n.
+        # We will check that each number in the row is in the list
+        # and remove the numbers from the list once they are verified
+        # to ensure that each number only occurs once in the row.
+        check_list = list(range(1, len(square[0]) + 1))
+        for i in row:
+            if i not in check_list:
+                return False
+            check_list.remove(i)
+    for n in range(len(square[0])):
+        # We do the same here for each column in the square.
+        check_list = list(range(1, len(square[0]) + 1))
+        for row in square:
+            if row[n] not in check_list:
+                return False
+            check_list.remove(row[n])
+    return True
+
+
+print(check_sudoku(correct))
+#>>> False
+
+print(check_sudoku(correct))
+#>>> True
+
+print(check_sudoku(incorrect2))
+#>>> False
+
+print(check_sudoku(incorrect3))
+#>>> False
+
+print(check_sudoku(incorrect4))
+#>>> False
+
+print(check_sudoku(incorrect5))
+#>>> False
+
+
